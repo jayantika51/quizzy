b312a03806a5d2b54550116973d704b839400611
jayantika51 <79147549+jayantika51@users.noreply.github.com>
2021-10-28 08:19:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function n(){
+    document.getElementById("mark_1").innerHTML='1';
+}
+function k(){
+    document.getElementById("mark2").innerHTML='1';
+}
+function o(){
+    document.getElementById("mark3").innerHTML='1';
+}
+function c(){
+    document.getElementById("mark4").innerHTML='1';
+}
+function f(){
+    document.getElementById("mark5").innerHTML='1';
+}
+
+
+
+function no(){
+    document.getElementById("mark_1").innerHTML='X';
+}
+function l(){
+    document.getElementById("mark2").innerHTML='X';
+}
+function w(){
+    document.getElementById("mark3").innerHTML='X';
+}
+function q(){
+    document.getElementById("mark4").innerHTML='X';
+}
+function r(){
+    document.getElementById("mark5").innerHTML='X';
+}
+
